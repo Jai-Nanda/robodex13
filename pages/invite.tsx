39f41dbb9f48e14f897380ebb3473ed3b3849177
PROG-007 <raghavadi007@gmail.com>
2023-08-24 10:56:01 +0530

    added redirect "invite"

---
+++ pages/invite.tsx
@@ -0,0 +1,6 @@
+import React from "react"
+export default function Discord() {
+  return (
+    <meta http-equiv="refresh" content="0;url=https://drive.google.com/file/d/1gYoNhqH537j4yWCohD4NqOzD-H4ZDtJd/view?usp=sharing" />
+  )
+}
