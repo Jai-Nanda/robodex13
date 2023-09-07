f00f7bfd190dc357a01f4b8e4e2bf60e0fb238d4
PROG-007 <raghavadi007@gmail.com>
2023-09-07 15:11:16 +0530

    added events coming soon

--- pages/events.tsx
+++ pages/events.tsx
@@ -1,10 +1,18 @@
 import Slider from './components/Slider'
+import undraw from '@/public/undraw.svg'
+import Image from 'next/image'
 export default function events() {
   return (
     <div>
       <h1 className="text-pm-color font-black text-center text-3xl">EVENTS@13</h1>
       <div className='mb-14'>
-        <Slider/>
+        {/* <Slider/> */}
+        <div className="flex justify-center items-center flex-col h-[60vh] ">
+          {/* <div className="flex justify-center items-center flex-col shadow-nav m-auto rounded-lg backdrop-filter backdrop-blur-sm bg-opacity-60 p-10"> */}
+
+          <Image src={undraw} alt='bitch' className='w-96 mb-10 '></Image>
+          <h1 className=" text-center text-pm-color font-black  text-2xl ">Comming Soon...</h1>
+        </div>
       </div>
     </div>
   )
