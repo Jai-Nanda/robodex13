1bd49bc0dbb1a3ca002dfd5a4319d3ec6cb0d2e5
PROG-007 <raghavadi007@gmail.com>
2023-09-09 12:00:24 +0530

    link change

--- pages/invite.tsx
+++ pages/invite.tsx
@@ -1,6 +1,6 @@
 import React from "react"
 export default function Discord() {
   return (
-    <meta http-equiv="refresh" content="0;url=https://drive.google.com/file/d/1gYoNhqH537j4yWCohD4NqOzD-H4ZDtJd/view?usp=sharing" />
+    <meta http-equiv="refresh" content="0;url=https://drive.google.com/file/d/1cZeOlCXMZ76jtyzHDV98F9uJqufc_H3B/view?usp=sharing" />
   )
 }
