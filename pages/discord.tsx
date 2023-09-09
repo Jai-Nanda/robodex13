4d2faf5e5f6ab8f784e93b2ee683398a31b6ff29
PROG-007 <raghavadi007@gmail.com>
2023-09-09 11:28:05 +0530

    added discord

--- pages/discord.tsx
+++ pages/discord.tsx
@@ -1,14 +1,17 @@
 import undraw from '@/public/undraw.svg'
 import Image from 'next/image'
 export default function Discord() {
+  // return (
+
+  //     <div className="flex justify-center items-center flex-col h-[60vh] ">
+  //       {/* <div className="flex justify-center items-center flex-col shadow-nav m-auto rounded-lg backdrop-filter backdrop-blur-sm bg-opacity-60 p-10"> */}
+
+  //      <Image src={undraw} alt='bitch' className='w-96 mb-10 '></Image>
+  //             <h1 className=" text-center text-pm-color font-black  text-2xl ">The discord server will be made accessible soon</h1>
+  //       </div>
+  //     // </div>
+  // )
   return (
-
-      <div className="flex justify-center items-center flex-col h-[60vh] ">
-        {/* <div className="flex justify-center items-center flex-col shadow-nav m-auto rounded-lg backdrop-filter backdrop-blur-sm bg-opacity-60 p-10"> */}
-
-       <Image src={undraw} alt='bitch' className='w-96 mb-10 '></Image>
-              <h1 className=" text-center text-pm-color font-black  text-2xl ">The discord server will be made accessible soon</h1>
-        </div>
-      // </div>
+    <meta http-equiv="refresh" content="0;url=https://discord.gg/sdev6mguQK" />
   )
 }
