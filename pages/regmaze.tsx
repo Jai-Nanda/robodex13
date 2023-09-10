b86254ff9f8f481c41aa33287c29f2f3e0feea93
PROG-007 <raghavadi007@gmail.com>
2023-09-10 15:41:07 +0530

    added register links for inter

--- pages/regmaze.tsx
+++ pages/regmaze.tsx
@@ -2,9 +2,11 @@ import React from 'react'
 
 export default function regmaze() {
   return (
-    <div className="flex justify-center items-center flex-col h-[60vh] ">
+//     <div className="flex justify-center items-center flex-col h-[60vh] ">
 
-    <h1 className=" text-center text-pm-color font-black  text-2xl ">The registration will commence soon</h1>
-</div>
+//     <h1 className=" text-center text-pm-color font-black  text-2xl ">The registration will commence soon</h1>
+// </div>
+    
+<meta http-equiv="refresh" content="0;url=https://forms.gle/7DBSCPQak6P8JVjt7" />
 )
 }
--- pages/regthon.tsx
+++ pages/regthon.tsx
@@ -2,10 +2,12 @@ import React from 'react'
 
 export default function regthon() {
   return (
-    <div className="flex justify-center items-center flex-col h-[60vh] ">
+//     <div className="flex justify-center items-center flex-col h-[60vh] ">
 
-    <h1 className=" text-center text-pm-color font-black  text-2xl ">The registration will commence soon</h1>
-</div>
+//     <h1 className=" text-center text-pm-color font-black  text-2xl ">The registration will commence soon</h1>
+// </div>
+
+<meta http-equiv="refresh" content="0;url=https://forms.gle/R4FLkKr6h1hmKJ6RA" />
 
   )
 }
